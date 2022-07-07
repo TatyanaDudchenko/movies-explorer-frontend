dacbe84898264c6f170cbe2ae2a6adc87a2a07ba
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-07-08 04:39:22 +0500

    feat: сверстать шапку сайта

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,20 @@
+import headerLogo from '../../images/logo.svg';
+import './Header.css';
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+
 function Header() {
-  
+    return (
+        <div className='header'>
+            <img className='header__logo' src={headerLogo} alt='Логотип Movies Explorer' />
+            <nav className='header__navbar'>
+                <button className='header__navbar_item header__navbar_item-register'>Регистрация</button>
+                <button className='header__navbar_item header__navbar_item-login'>Войти</button>
+            </nav>
+        </div>
+    )
 }
 
 export default Header;
