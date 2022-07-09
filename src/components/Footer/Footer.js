dbbaa6216dc38cc9731bca0a8b3e7a90349d206a
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-07-10 00:27:42 +0500

    fix: скорректировать наименование класса

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,12 +4,12 @@ import React from 'react';
 function Footer() {
     return (
         <div className='footer'>
-            <div class="footer__copyright">
+            <div className="footer__copyright">
                 <p className='footer__copyright_info'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
             </div>
-            <div class='footer__content'>
+            <div className='footer__content'>
                 <p className='footer__date'>&copy; 2022</p>
-                <nav class='footer__social'>
+                <nav className='footer__social'>
                     <button className='footer__social_link'>Яндекс.Практикум</button>
                     <button className='footer__social_link'>Github</button>
                     <button className='footer__social_link'>Facebook</button>
