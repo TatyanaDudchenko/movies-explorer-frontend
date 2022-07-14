a4a586842cbad4ed11e0177daffe2390fa722594
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-07-15 02:14:23 +0500

    feat: настроить стили и адаптив для компонента AboutProject

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -4,7 +4,9 @@ import React from 'react';
 function AboutProject() {
     return (
         <section className='about-project'>
-            <h2 className='about-project__title'>О проекте</h2>
+            <div className='about-project__title-container'>
+                <h2 className='about-project__title'>О проекте</h2>
+            </div>
             <div className='about-project__description'>
                 <div className='about-project__description-column'>
                     <h3 className='about-project__description-column_title'>Дипломный проект включал 5 этапов</h3>
@@ -16,17 +18,17 @@ function AboutProject() {
                 </div>
             </div>
             <div className='about-project__presentation'>
-                <div className='about-project__presentation-column about-project__presentation-column_left'>
-                    <div className='about-project__presentation-column-item about-project__presentation-column-item_title-color-left'>
-                        <h4 className='about-project__presentation-column-title_left'>1 неделя</h4>
+                <div className='about-project__presentation-column about-project__presentation-column_first'>
+                    <div className='about-project__presentation-column-item about-project__presentation-column-item_color-first'>
+                        <h4 className='about-project__presentation-column-title about-project__presentation-column-title_first'>1 неделя</h4>
                     </div>
                     <div className='about-project__presentation-column-item'>
                         <p className='about-project__presentation-column-paragraph'>Back-end</p>
                     </div>
                 </div>
-                <div className='about-project__presentation-column about-project__presentation-column_right'>
-                    <div className='about-project__presentation-column-item about-project__presentation-column-item_title-color-right'>
-                        <h4 className='about-project__presentation-column-title_right'>4 недели</h4>
+                <div className='about-project__presentation-column about-project__presentation-column_second'>
+                    <div className='about-project__presentation-column-item about-project__presentation-column-item_color-second'>
+                        <h4 className='about-project__presentation-column-title'>4 недели</h4>
                     </div>
                     <div className='about-project__presentation-column-item'>
                         <p className='about-project__presentation-column-paragraph'>Front-end</p>
