de3537ebd7e13b2c797c43484e997c5c9cdb79a8
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-07-16 04:23:38 +0500

    feat: сверстать компонент Techs и настроить адаптив

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -2,21 +2,24 @@ import './Techs.css';
 
 function Techs() {
     return (
-        <div className=''>
+        <div className='techs-container'>
             <div className='title-container'>
                 <h2 className='title-container__title'>Технологии</h2>
             </div>
-            <h3>7 технологий</h3>
-            <p>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JS</li>
-                <li>React</li>
-                <li>Git</li>
-                <li>Express.js</li>
-                <li>MongoDB</li>
-            </ul>
+            <div className='techs-content'>
+                <h3 className='techs-content__title'>7 технологий</h3>
+                <p className='techs-content__paragraph'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+                <ul className='techs-content__list'>
+                    <li className='techs-content__item'>HTML</li>
+                    <li className='techs-content__item'>CSS</li>
+                    <li className='techs-content__item'>JS</li>
+                    <li className='techs-content__item'>React</li>
+                    <li className='techs-content__item'>Git</li>
+                    <li className='techs-content__item'>Express.js</li>
+                    <li className='techs-content__item'>MongoDB</li>
+                </ul>
+            </div>
+
         </div>
     )
 }
