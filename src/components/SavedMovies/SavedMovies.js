9f9bad5c422ebb04e0c2413b571f0a6aaba12b47
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-08-06 22:21:26 +0500

    feat: доработать компонент страницы с сохранёнными карточками фильмов

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,5 +1,12 @@
+import './SavedMovies.css';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+
 function SavedMovies() {
-  
+    return (
+        <div className='content'>
+            <MoviesCardList />
+        </div>
+    )
 }
 
 export default SavedMovies;
