67fa872f0afd864efffd14e34ac426ae1adb674b
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-08-13 02:55:23 +0500

    feat: доработать компоненты навигации

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,31 +1,31 @@
 import './Navigation.css';
-import { useLocation } from 'react-router-dom';
+import { useLocation, NavLink } from 'react-router-dom';
 
 function Navigation({ isAuth }) {
 
     let location = useLocation();
 
     return (
-        <div>
+        <div className='header__navbar-container'>
             {!isAuth &&
                 <nav className='header__navbar header__navbar_justify-content_end'>
-                    <button className='header__navbar_item header__navbar_item-register'>Регистрация</button>
-                    <button className='header__navbar_item header__navbar_item-login'>Войти</button>
+                    <NavLink to='/signup' className='header__navbar_item header__navbar_item-register'>Регистрация</NavLink>
+                    <NavLink to='/signin' className='header__navbar_item header__navbar_item-login'>Войти</NavLink>
                 </nav>
             }
             {isAuth &&
                 <nav className='header__navbar header__navbar_justify-content_space-between'>
                     <div className='header__navbar_item-group'>
-                        <button
+                        <NavLink to='/movies'
                             className={`header__navbar_item header__navbar_item-movies
                             ${location.pathname.includes('/movies') && 'header__navbar_item_current'}`}>
                             Фильмы
-                        </button>
-                        <button
+                        </NavLink>
+                        <NavLink to='/saved-movies'
                             className={`header__navbar_item header__navbar_item-movies
                             ${location.pathname.includes('/saved-movies') && 'header__navbar_item_current'}`}>
                             Сохраненные фильмы
-                        </button>
+                        </NavLink>
                     </div>
 
                     <button className='header__navbar_item header__navbar_item-account'>Аккаунт</button>
