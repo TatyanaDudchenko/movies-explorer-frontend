40422884716d56fbb55a68e0b2684ef2c708b6a5
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-08-14 04:26:08 +0500

    feat: доработать компонент формы поиска

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,9 +3,22 @@ import './SearchForm.css';
 
 function SearchForm() {
     return (
-        <form className='search-form'>
-            <h2>Фильм</h2>
-        </form>
+        <div className='search-form-container'>
+            <form className='search-form'>
+                <input className='search-form__input'
+                    type='text'
+                    name='find'
+                    placeholder='Фильм'
+                    required
+                />
+                <button type='submit' className='search-form__button search-form__submit-button'></button>
+            </form>
+            <div className='search-form__toggle-container'>
+                <button type='switch' className='search-form__button search-form__toggle-icon'></button>
+                <p className='search-form__toggle-text'>Короткометражки</p>
+            </div>
+        </div>
+
     )
 }
 
