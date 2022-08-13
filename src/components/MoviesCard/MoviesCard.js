3d02e666ad4c269b37ccc87cb69f6eaefe9382de
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-08-14 02:12:00 +0500

    feat: доработать компоненты для отображения карточек фильмов

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,26 +1,20 @@
 import './MoviesCard.css';
-// import moviesCardImage from '../../images/image-movies-card-example-big.png'; // тестовый пример
-import moviesCardImage from '../../images/image-movies-card-example-small.PNG'; // тестовый пример
-import iconSavedInactive from '../../images/icon-saved-inactive.svg';
 
-
-function MoviesCard() {
+function MoviesCard({ moviesCard }) {
 
     return (
         <figure className='movies-card'>
             <div className='movies-card__container'>
                 <figcaption className='movies-card__parameters'>
                     <div className='movies-card__about'>
-                        <h2 className='movies-card__about-name'>33 слова о дизайне</h2>
+                        <h2 className='movies-card__about-name'>{moviesCard.name}</h2>
                         <p className='movies-card__about-duration'>1ч 47мин</p>
                     </div>
-                    <button type='button'>
-                        <img className='movies-card__like' src={iconSavedInactive} alt='Иконка сохранения фильма' />
-                    </button>
+                    <button type='button' className='movies-card__icon-like' alt='Иконка сохранения фильма'></button>
                 </figcaption>
                 <div className='movies-card__image-container'>
 
-                <img className='movies-card__image' src={moviesCardImage} alt='Обложка фильма' />
+                <img className='movies-card__image' src={moviesCard.link} alt={moviesCard.name} />
 
                 </div>
             </div>
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,21 +1,74 @@
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
+import moviesCardImage01 from '../../images/image-movies-card-example-01.png'; // тестовый пример
+import moviesCardImage02 from '../../images/image-movies-card-example-02.png'; // тестовый пример
+import moviesCardImage03 from '../../images/image-movies-card-example-03.png'; // тестовый пример
+import moviesCardImage04 from '../../images/image-movies-card-example-04.png'; // тестовый пример
+import moviesCardImage05 from '../../images/image-movies-card-example-05.png'; // тестовый пример
+import moviesCardImage06 from '../../images/image-movies-card-example-06.png'; // тестовый пример
+import moviesCardImage07 from '../../images/image-movies-card-example-07.png'; // тестовый пример
+import moviesCardImage08 from '../../images/image-movies-card-example-08.png'; // тестовый пример
+import moviesCardImage09 from '../../images/image-movies-card-example-09.png'; // тестовый пример
+import moviesCardImage10 from '../../images/image-movies-card-example-10.png'; // тестовый пример
+import moviesCardImage11 from '../../images/image-movies-card-example-11.png'; // тестовый пример
+import moviesCardImage12 from '../../images/image-movies-card-example-12.png'; // тестовый пример
 
 function MoviesCardList() {
+
+    const moviesCardsArray = [
+        {
+            name: '33 слова о дизайне',
+            link: moviesCardImage01
+        },
+        {
+            name: '33 слова о дизайне',
+            link: moviesCardImage02
+        },
+        {
+            name: '33 слова о дизайне',
+            link: moviesCardImage03
+        },
+        {
+            name: '33 слова о дизайне',
+            link: moviesCardImage04
+        },
+        {
+            name: '33 слова о дизайне',
+            link: moviesCardImage05
+        },
+        {
+            name: '33 слова о дизайне',
+            link: moviesCardImage06
+        },
+        {
+            name: '33 слова о дизайне',
+            link: moviesCardImage07
+        },
+        {
+            name: '33 слова о дизайне',
+            link: moviesCardImage08
+        },
+        {
+            name: '33 слова о дизайне',
+            link: moviesCardImage09
+        },
+        {
+            name: '33 слова о дизайне',
+            link: moviesCardImage10
+        },
+        {
+            name: '33 слова о дизайне',
+            link: moviesCardImage11
+        },
+        {
+            name: '33 слова о дизайне',
+            link: moviesCardImage12
+        }
+    ];
+
     return (
         <div className='movies-card-list'>
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
+            {moviesCardsArray.map(item => (<MoviesCard key={item._id} {...item} moviesCard={item} />))}
         </div>
     )
 }
