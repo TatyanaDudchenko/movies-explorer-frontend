38175f0d52ef911929e0d323a395d6c457057671
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-08-17 03:02:36 +0500

    feat: релизовать отображение кнопки меню

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,32 +6,37 @@ function Navigation({ isAuth }) {
     let location = useLocation();
 
     return (
-        <div className='header__navbar-container'>
+        <>
             {!isAuth &&
                 <nav className='header__navbar header__navbar_justify-content_end'>
                     <NavLink to='/signup' className='header__navbar_item header__navbar_item-register'>Регистрация</NavLink>
                     <NavLink to='/signin' className='header__navbar_item header__navbar_item-login'>Войти</NavLink>
                 </nav>
             }
-            {isAuth &&
-                <nav className='header__navbar header__navbar_justify-content_space-between'>
-                    <div className='header__navbar_item-group'>
-                        <NavLink to='/movies'
-                            className={`header__navbar_item header__navbar_item-movies
-                            ${location.pathname.includes('/movies') && 'header__navbar_item_current'}`}>
-                            Фильмы
-                        </NavLink>
-                        <NavLink to='/saved-movies'
-                            className={`header__navbar_item header__navbar_item-movies
-                            ${location.pathname.includes('/saved-movies') && 'header__navbar_item_current'}`}>
-                            Сохраненные фильмы
-                        </NavLink>
-                    </div>
 
-                    <NavLink exact to='/' className='header__navbar_item header__navbar_item-account'>Аккаунт</NavLink>
-                </nav>
+            {isAuth &&
+                <>
+                    <div className='header__menu'>
+                        <div className='header__menu-button'></div>
+                    </div>
+                    <nav className='header__navbar header__navbar_justify-content_space-between header__navbar_inactive'>
+                        <div className='header__navbar_item-group'>
+                            <NavLink to='/movies'
+                                className={`header__navbar_item header__navbar_item-movies
+                            ${location.pathname.includes('/movies') && 'header__navbar_item_current'}`}>
+                                Фильмы
+                            </NavLink>
+                            <NavLink to='/saved-movies'
+                                className={`header__navbar_item header__navbar_item-movies
+                            ${location.pathname.includes('/saved-movies') && 'header__navbar_item_current'}`}>
+                                Сохраненные фильмы
+                            </NavLink>
+                        </div>
+                        <NavLink exact to='/' className='header__navbar_item header__navbar_item-account'>Аккаунт</NavLink>
+                    </nav>
+                </>
             }
-        </div>
+        </>
     )
 }
 
