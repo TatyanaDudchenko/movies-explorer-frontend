05acc080a9d55718214245db021cd77c41600118
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-08-22 03:31:34 +0500

    feat: настроить параметры карточки фильма

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,21 +1,29 @@
+// import { Link } from 'react-router-dom';
 import './MoviesCard.css';
 
 function MoviesCard({ movie }) {
+
+    const moviesUrl = 'https://api.nomoreparties.co/';
+
+    // преобразование минут в часы и минуты
+    const time = movie.duration
+    const hours = Math.trunc(time/60);
+    const minutes = time % 60;
+    const moviesTime = hours + 'ч ' + minutes + 'м';
 
     return (
         <figure className='movies-card'>
             <div className='movies-card__container'>
                 <figcaption className='movies-card__parameters'>
                     <div className='movies-card__about'>
-                        <h2 className='movies-card__about-name'>{movie.name}</h2>
-                        <p className='movies-card__about-duration'>1ч 47мин</p>
+                        <h2 className='movies-card__about-name'>{movie.nameRU}</h2>
+                        <time className='movies-card__about-duration'>{moviesTime}</time>
                     </div>
                     <button type='button' className='movies-card__icon-like' alt='Иконка сохранения фильма'></button>
                 </figcaption>
                 <div className='movies-card__image-container'>
-
-                <img className='movies-card__image' src={movie.link} alt={movie.name} />
-
+                    <img className='movies-card__image' src={`${moviesUrl}${movie.image.url}`} alt={movie.name} />
+                    {/* <Link to={`${movie.trailerLink}`}><img className='movies-card__image' src={`${moviesUrl}${movie.image.url}`} alt={movie.name} /></Link> */}
                 </div>
             </div>
 
