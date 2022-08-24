807888de31e21343107456ed307cf48c7c80b7cf
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-08-25 01:11:44 +0500

    feat: реализовать функциональность для регистрации

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -5,25 +5,47 @@ import FormInput from '../FormInput/FormInput';
 import headerLogo from '../../images/logo.svg';
 import FormSubmitButton from '../FormSubmitButton/FormSubmitButton';
 import FormParagraphLinkContainer from '../FormParagraphLinkContainer/FormParagraphLinkContainer';
+import { useState } from 'react';
 
-function Register({ error }) {
+function Register({ error, handleRegister }) {
+
+    const [registerState, setRegisterState] = useState({
+        password: '',
+        email: '',
+        userName: '',
+    });
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setRegisterState(old => ({
+            ...old,
+            [name]: value
+        }));
+    };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        handleRegister(registerState);
+    }; 
+
     return (
         <div className='register-form register-form__container'>
             <img className='register-form__logo' src={headerLogo} alt='Логотип Movies Explorer' />
-            <form className='register-form__content' noValidate>
+            <form onSubmit={handleSubmit} className='register-form__content' noValidate>
                 <div className='register-form__title register-form__title_margin'>
                     <FormTitle title={'Добро пожаловать!'} />
                 </div>
                 <div className='register-form__content_align'>
                     <div className='register-form__fields'>
                         <FormField label='Имя'>
-                            <FormInput error={error} type='text' name='name' minLength='2' maxLength='30' />
+                            <FormInput onChange={handleChange} error={error} type='text' name='UserName' value={registerState.userName} minLength='2' maxLength='30' />
                         </FormField>
                         <FormField label='E-mail'>
-                            <FormInput error={error} type='email' name='email' minLength='2' maxLength='30' />
+                            <FormInput onChange={handleChange} error={error} type='email' name='email' value={registerState.email} minLength='2' maxLength='30' />
                         </FormField>
                         <FormField label='Пароль' error={error}>
-                            <FormInput error={error} type='password' name='password' />
+                            <FormInput onChange={handleChange} error={error} type='password' name='password' value={registerState.password} />
                         </FormField>
                     </div>
                     <div className='register-form__button-container'>
