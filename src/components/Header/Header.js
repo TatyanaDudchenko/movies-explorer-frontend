6438b123f5cca5c93310a9ddf3962b3ff2499b66
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-08-25 01:09:21 +0500

    fix: скорректировать наименования параметров

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,14 @@ import './Header.css';
 import Navigation from '../Navigation/Navigation';
 import { useLocation } from 'react-router-dom';
 
-function Header({ isAuth, onMenuPopup }) {
+function Header({ signedIn, onMenuPopup }) {
 
     let location = useLocation();
 
     return (
         <div className={`header ${(location.pathname.includes('/error-page')) && 'header_inactive'}`}>
             <img className='header__logo' src={headerLogo} alt='Логотип Movies Explorer' />
-            <Navigation isAuth={isAuth} onMenuPopup={onMenuPopup} />
+            <Navigation signedIn={signedIn} onMenuPopup={onMenuPopup} />
         </div>
     )
 }
