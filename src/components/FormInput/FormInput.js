cd07dd29f221a55c4d3e710130f3cbae68561b72
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-08-25 01:14:25 +0500

    fix: скорректировать передаваемые параметры

--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -1,9 +1,10 @@
 import './FormInput.css';
 
-function FormInput({ name, type, minLength, maxLength, error }) {
+    function FormInput({ name, type, minLength, maxLength, error, onChange }) { // value
     return (
         <div className='form-input'>
             <input className={`form-input__input ${error=false && 'form-input__error-message_color'}`}
+                onChange={onChange}
                 type={type}
                 name={name}
                 minLength={minLength}
