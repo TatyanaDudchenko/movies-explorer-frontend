e5456d18076fbd991a03c7a6850c8d63ebd86d25
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-09-09 02:43:54 +0500

    fix: скорректировать адрес сервера

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL = 'https://api.movies-explorer-dudta.nomoreparties.sbs';
 
 const checkResponse = (response) => {
   if (response.ok) {
