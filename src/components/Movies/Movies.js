de9e2264ef36305230ec871ed1a3c5f8ef95bf7d
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-09-17 04:22:10 +0500

    feat: доработать функциональность клика по кнопке Еще в зависимости от размера экрана

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -3,19 +3,49 @@ import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import { useState, useEffect } from 'react';
 
-function Movies({ movies, filteredMovies, moviesSearchResult, setMoviesSearchResult, savedMovies, moviesUrl, onGetFoundMovies, onToggleClick, onToggleClickState, onSetIsToggleClick, onLikeClickState, onMovieLike, onSearchAndFilterMovies }) {
+function Movies({
+    movies,
+    filteredMovies,
+    moviesSearchResult,
+    setMoviesSearchResult,
+    savedMovies,
+    moviesUrl,
+    onGetFoundMovies,
+    onToggleClick,
+    onToggleClickState,
+    onSetIsToggleClick,
+    onLikeClickState,
+    onMovieLike,
+    onSearchAndFilterMovies
+}) {
 
     const [searchText, setSearchText] = useState('');
     const [showMovies, setShowMovies] = useState([]);
-    const NUMBER_OF_MOVIES_TO_SHOW = 12;
+    const [isMoreButtonActive, setMoreButtonActive] = useState(false);
+
+    const [numberOfMoviesToShow, setnumberOfMoviesToShow] = useState(12);
+    const [numberOfMoviesToShowAdd, setNumberOfMoviesToShowAdd] = useState(3);
+
+    const moreButtonActiveClassName = 'movies-container__more-button_active';
+
+    const SIZE_DESKTOP = 1280;
+    const SIZE_TABLET = 768;
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    function changeWidth() {
+        setWindowWidth(window.innerWidth);
+    }
 
     function handleMoreButtonClick() {
-        setShowMovies(showMovies => moviesSearchResult.slice(0, showMovies.length + NUMBER_OF_MOVIES_TO_SHOW))
+        setShowMovies(showMovies => moviesSearchResult.slice(0, showMovies.length + numberOfMoviesToShowAdd));
+        if (showMovies.length >= moviesSearchResult.length - numberOfMoviesToShowAdd) {
+            setMoreButtonActive(false);
+        }
     }
 
     useEffect(() => {
-        setShowMovies(moviesSearchResult.slice(0, NUMBER_OF_MOVIES_TO_SHOW));
-    }, [moviesSearchResult]);
+        setShowMovies(moviesSearchResult.slice(0, numberOfMoviesToShow));
+    }, [moviesSearchResult, numberOfMoviesToShow]);
 
 
     useEffect(() => {
@@ -24,6 +54,41 @@ function Movies({ movies, filteredMovies, moviesSearchResult, setMoviesSearchRes
         setSearchText(localStorageSearchText);
     }, []);
 
+    useEffect(() => {
+        if (moviesSearchResult.length <= numberOfMoviesToShow) {
+            setMoreButtonActive(false);
+        } else {
+            setMoreButtonActive(true);
+        }
+    }, [moviesSearchResult.length, numberOfMoviesToShow]);
+
+    useEffect(() => {
+        if (moviesSearchResult.length <= numberOfMoviesToShow) {
+            setMoreButtonActive(false);
+        } else {
+            setMoreButtonActive(true);
+        }
+    }, [moviesSearchResult.length, numberOfMoviesToShow]);
+
+    useEffect(() => {
+        window.addEventListener("resize", changeWidth);
+        return () => {
+            window.removeEventListener("resize", changeWidth);
+        };
+    });
+
+    useEffect(() => {
+        if (windowWidth >= SIZE_DESKTOP) {
+            setnumberOfMoviesToShow(12);
+            setNumberOfMoviesToShowAdd(3);
+        } else if (windowWidth < SIZE_DESKTOP && windowWidth > SIZE_TABLET) {
+            setnumberOfMoviesToShow(8);
+            setNumberOfMoviesToShowAdd(2);
+        } else {
+            setnumberOfMoviesToShow(5);
+            setNumberOfMoviesToShowAdd(2);
+        }
+    }, [windowWidth]);
 
     return (
         <div className='movies-container'>
@@ -39,8 +104,6 @@ function Movies({ movies, filteredMovies, moviesSearchResult, setMoviesSearchRes
                 setSearchTextQuery={setSearchText}
                 setSearchText={setSearchText}
                 keyInStorageSearchText={'searchText'}
-
-                // onSearchAndFilterMovies={onSearchAndFilterMovies} />
                 onSearchAndFilterFunction={onSearchAndFilterMovies} />
             <MoviesCardList
                 moviesUrl={moviesUrl}
@@ -52,8 +115,12 @@ function Movies({ movies, filteredMovies, moviesSearchResult, setMoviesSearchRes
                 onLikeClickState={onLikeClickState}
                 onMovieLike={onMovieLike} />
             <div className='movies-container__more-button-container'>
-                {/* <button type='button' className='movies-container__more-button'>Еще</button> */}
-                <button onClick={handleMoreButtonClick} type='button' className='movies-container__more-button'>Еще</button>
+                <button
+                    onClick={handleMoreButtonClick}
+                    type='button'
+                    className={`movies-container__more-button ${isMoreButtonActive && moreButtonActiveClassName}`}>
+                    Еще
+                </button>
             </div>
         </div>
     )
